440faa1ed48d9f130534ee6fab6dd5970c9a73f7
gaoxiong <xgao@dayuwuxian.com>
2016-06-12 12:46:53 +0800

    wenjinhai

--- app/src/main/java/com/snaptube/interview/MainActivity.java
+++ app/src/main/java/com/snaptube/interview/MainActivity.java
@@ -1,13 +1,30 @@
 package com.snaptube.interview;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+  private Button mButton1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    initView();
+  }
+
+  private void initView() {
+    mButton1 = (Button) findViewById(R.id.button1);
+    mButton1.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 }
